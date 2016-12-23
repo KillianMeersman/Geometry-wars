b777dad17b0aa989e0733b15a1e126724e9081e6
Chronoxx <deneefrobin@gmail.com>
2016-12-23 21:01:26 +0100

    Register Screen
    Tadaaaa

--- Main/src/howest/groep14/game/screens/RegisterScreen.java
+++ Main/src/howest/groep14/game/screens/RegisterScreen.java
@@ -3,11 +3,15 @@ package howest.groep14.game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import howest.groep14.game.CustomUtils;
 
 public class RegisterScreen implements Screen {
     private Stage stage;
@@ -41,6 +45,42 @@ public class RegisterScreen implements Screen {
         usernameField.setHeight(20);
         usernameField.setPosition(x_center - 250, height - 50);
         stage.addActor(usernameField);
+
+        Label emailLabel = new Label("Email:", skin);
+        emailLabel.setWidth(label_width);
+        emailLabel.setHeight(20);
+        emailLabel.setPosition(x_center - 350, height - 100);
+        stage.addActor(emailLabel);
+
+        TextField emailField = new TextField("", skin);
+        emailField.setWidth(500);
+        emailField.setHeight(20);
+        emailField.setPosition(x_center - 250, height - 100);
+        stage.addActor(emailField);
+
+        Label passwordLabel = new Label("Password:", skin);
+        passwordLabel.setWidth(label_width);
+        passwordLabel.setHeight(20);
+        passwordLabel.setPosition(x_center - 350, height - 150);
+        stage.addActor(passwordLabel);
+
+        TextField passwordField = new TextField("", skin);
+        passwordField.setWidth(500);
+        passwordField.setHeight(20);
+        passwordField.setPosition(x_center - 250, height - 150);
+        stage.addActor(passwordField);
+
+        TextButton quitButton = CustomUtils.generateTextButton(skin, "R E G I S T E R", x_center - 350, height - 250, 650, 50);
+        stage.addActor(quitButton);
+        quitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                doRegister();
+            }
+        });
+    }
+
+    private void doRegister() {
     }
 
     @Override
